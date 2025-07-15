17c8f12ec02a139fee46eb5a97559728e92326c5
son.nguyentruong1 <sonqp1999a@gmail.com>
2025-07-15 17:47:07 +0700

    add tuyển thủ

---
+++ data/data.ts
@@ -0,0 +1,166 @@
+export const data = [
+  {
+    name: "Magnus Dai",
+    elo: 1200,
+    location: "Nghi Tân",
+    badge: "World Champion",
+    img: "https://imgproxy.mezon.ai/K0YUZRIosDOcz5lY6qrgC6UIXmQgWzLjZv7VJ1RAA8c/rs:fit:100:100:1/mb:2097152/plain/https://cdn.mezon.ai/1779484504377790464/1783440675396653056/1783440675396653000/1745823516989_undefined1745822891637_0image.png_croppedWEBP@webp",
+  },
+  {
+    name: "Vladimir Huy",
+    elo: 1150,
+    location: "Nghi Trường",
+    badge: "Legendary",
+    img: "https://imgproxy.mezon.ai/K0YUZRIosDOcz5lY6qrgC6UIXmQgWzLjZv7VJ1RAA8c/rs:fill:529:543:1/mb:2097152/plain/https://cdn.mezon.ai/0/1840683727907196928/1810223235401257000/1752575386989_image.png",
+  },
+  {
+    name: "Judit Hang",
+    elo: 1200,
+    location: "Vinh",
+    badge: "World Champion",
+    img: "https://imgproxy.mezon.ai/K0YUZRIosDOcz5lY6qrgC6UIXmQgWzLjZv7VJ1RAA8c/rs:fit:100:100:1/mb:2097152/plain/https://cdn.mezon.vn/1779484504377790464/1831537368687972352/1783442595926511600/1298092AFAB_A0C1_4A98_BC20_5CAE7FA5F7EE.jpg@webp",
+  },
+  {
+    name: "Ang Tuan Nakamura",
+    elo: 680,
+    location: "Quang Trung",
+    badge: "Speed Master",
+    img: "https://imgproxy.mezon.ai/K0YUZRIosDOcz5lY6qrgC6UIXmQgWzLjZv7VJ1RAA8c/rs:fit:100:100:1/mb:2097152/plain/https://cdn.mezon.ai/1779484504377790464/1783441468711505920/1783441468711506000/1745830367712_undefined1745829414052_0image.png_croppedWEBP@webp",
+  },
+  {
+    name: "Khắc Vinh",
+    elo: 850,
+    location: "Bến Thủy",
+    badge: "Challenger",
+    img: "https://imgproxy.mezon.ai/K0YUZRIosDOcz5lY6qrgC6UIXmQgWzLjZv7VJ1RAA8c/rs:fit:100:100:1/mb:2097152/plain/https://cdn.mezon.vn/1779484504377790464/1783444369026584576/1783444369026584600/744_undefinedimage2.WEBP@webp",
+  },
+
+  {
+    name: "Tai Cu",
+    elo: 800,
+    location: "Vinh Tân",
+    badge: "Challenger",
+    img: "https://imgproxy.mezon.ai/K0YUZRIosDOcz5lY6qrgC6UIXmQgWzLjZv7VJ1RAA8c/rs:fit:100:100:1/mb:2097152/plain/https://cdn.mezon.ai/1779484504377790464/1826122746413715456/1826122746413715500/1751525644861_ChatGPT_Image_Jul_3__2025__01_53_46_PM.png@webp",
+  },
+  {
+    name: "Ding Hoang",
+    elo: 800,
+    location: "Phan Đăng Lưu",
+    badge: "Rising Star",
+    img: "https://imgproxy.mezon.ai/K0YUZRIosDOcz5lY6qrgC6UIXmQgWzLjZv7VJ1RAA8c/rs:fit:100:100:1/mb:2097152/plain/https://cdn.mezon.ai/0/1783443441674358784/1783443441674358800/1751525628382_avatar_1.png@webp",
+  },
+  {
+    name: "Rose Que Phong",
+    elo: 750,
+    location: "Quế Phong",
+    badge: "Rising Star",
+    img: "https://imgproxy.mezon.ai/K0YUZRIosDOcz5lY6qrgC6UIXmQgWzLjZv7VJ1RAA8c/rs:fit:100:100:1/mb:2097152/plain/https://cdn.mezon.vn/0/0/1810223235401257000/174123270612010412696_F268_489E_839F_B6E757FE5A66.jpg@webp",
+  },
+  {
+    name: "Le Quang Hai",
+    elo: 750,
+    location: "Nam Đàn",
+    badge: "Speed Master",
+    img: "https://imgproxy.mezon.ai/K0YUZRIosDOcz5lY6qrgC6UIXmQgWzLjZv7VJ1RAA8c/rs:fit:100:100:1/mb:2097152/plain/https://cdn.mezon.ai/1779484504377790464/1807983162756698112/1807983162756698000/1745830329389_undefinedScreenshot_2025_04_28_155048.png_croppedWEBP@webp",
+  },
+  {
+    name: "Fabiano Thin",
+    elo: 400,
+    location: "Nghi Quang",
+    badge: "Challenger",
+    img: "https://imgproxy.mezon.ai/K0YUZRIosDOcz5lY6qrgC6UIXmQgWzLjZv7VJ1RAA8c/rs:fit:100:100:1/mb:2097152/plain/https://cdn.mezon.vn/1779484504377790464/1840669672182124544/1840669672182124500/1739780981845_undefinedbnQzxQp3gkECR1p2LRYc_646f83049e8e4_cvtpl.WEBP@webp",
+  },
+  {
+    name: "Đinh Lập Tình",
+    elo: 400,
+    location: "Hưng Nguyên",
+    badge: "Rising Star",
+    img: "https://imgproxy.mezon.ai/K0YUZRIosDOcz5lY6qrgC6UIXmQgWzLjZv7VJ1RAA8c/rs:fit:100:100:1/mb:2097152/plain/https://cdn.mezon.ai/1779484504377790464/1840694295972024320/1930090353453437000/1752286245533IMG_3685.JPG@webp",
+  },
+
+  {
+    name: "Van Dat",
+    elo: 500,
+    location: "Hương Khê",
+    badge: "Rising Star",
+    img: "https://imgproxy.mezon.ai/K0YUZRIosDOcz5lY6qrgC6UIXmQgWzLjZv7VJ1RAA8c/rs:fit:100:100:1/mb:2097152/plain/https://cdn.mezon.ai/0/0/1840678415796015000/1752461033500IMG_0329.JPG@webp",
+  },
+
+  {
+    name: "Xuan Quan",
+    elo: 950,
+    location: "Can Lộc",
+    badge: "Rising Star",
+    img: "https://imgproxy.mezon.ai/K0YUZRIosDOcz5lY6qrgC6UIXmQgWzLjZv7VJ1RAA8c/rs:fit:100:100:1/mb:2097152/plain/https://cdn.mezon.vn/0/1838043036462878720/1833044269397446700/244_undefined9b08d6bedeba67e43eab.jpg@webp",
+  },
+
+  {
+    name: "Dinh Hieu",
+    elo: 950,
+    location: "Hưng Nguyên",
+    badge: "Rising Star",
+    img: "https://imgproxy.mezon.ai/K0YUZRIosDOcz5lY6qrgC6UIXmQgWzLjZv7VJ1RAA8c/rs:fit:100:100:1/mb:2097152/plain/https://cdn.mezon.vn/1779484504377790464/1831893186792919040/1783441990394843100/701_undefinedHieu_Nguyen_inh_2024_01_03T08_12_24.251_0.404239.jpg@webp",
+  },
+
+  {
+    name: "UchiHa Duc Anh Obito",
+    elo: 650,
+    location: "Quỳnh Lưu",
+    badge: "Rising Star",
+    img: "https://imgproxy.mezon.ai/K0YUZRIosDOcz5lY6qrgC6UIXmQgWzLjZv7VJ1RAA8c/rs:fit:100:100:1/mb:2097152/plain/https://cdn.mezon.ai/1779484504377790464/1783438504739475456/1783438504739475500/1751611602476_17409847869083914A782_76F8_4FA0_B00D_8736B920E2B4.jpg@webp",
+  },
+
+  {
+    name: "Hatake Van Thinh Kakashi",
+    elo: 600,
+    location: "Bến Thuỷ",
+    badge: "Rising Star",
+    img: "https://imgproxy.mezon.ai/K0YUZRIosDOcz5lY6qrgC6UIXmQgWzLjZv7VJ1RAA8c/rs:fit:100:100:1/mb:2097152/plain/https://cdn.mezon.vn/1779484504377790464/1831531260632109056/1783443471286145000/100_undefinedavatar.webp@webp",
+  },
+
+  {
+    name: "Duc Chinh",
+    elo: 600,
+    location: "Quỳnh Lưu",
+    badge: "Rising Star",
+    img: "https://imgproxy.mezon.ai/K0YUZRIosDOcz5lY6qrgC6UIXmQgWzLjZv7VJ1RAA8c/rs:fit:100:100:1/mb:2097152/plain/https://cdn.mezon.vn/1779484504377790464/1841678004955123712/1823998475507863600/589_undefinedCh_nh_Nguy_n___c_2024_03_01T08_41_53.256_0.518934.jpg@webp",
+  },
+
+  {
+    name: "Cong Dung",
+    elo: 650,
+    location: "Nghi Xuân",
+    badge: "Rising Star",
+    img: "https://imgproxy.mezon.ai/K0YUZRIosDOcz5lY6qrgC6UIXmQgWzLjZv7VJ1RAA8c/rs:fit:100:100:1/mb:2097152/plain/https://cdn.mezon.ai/1779484504377790464/1929365076809093120/1929365076809093000/1748832367054_undefined2025_06_02_09h45_29.png_croppedWEBP@webp",
+  },
+
+  {
+    name: "Viet Hoang",
+    elo: 800,
+    location: "Anh Son",
+    badge: "Rising Star",
+    img: "https://imgproxy.mezon.ai/K0YUZRIosDOcz5lY6qrgC6UIXmQgWzLjZv7VJ1RAA8c/rs:fit:100:100:1/mb:2097152/plain/https://cdn.mezon.vn/1779484504377790464/1831525207425159168/1831540707106492400/144_undefinedHoang_Nguyen_Viet_2024_08_09T08_35_04.083_0.602385.jpg@webp",
+  },
+  {
+    name: "Hong Man",
+    elo: 350,
+    location: "Nghi Hải",
+    badge: "Rising Star",
+    img: "https://imgproxy.mezon.ai/K0YUZRIosDOcz5lY6qrgC6UIXmQgWzLjZv7VJ1RAA8c/rs:fit:100:100:1/mb:2097152/plain/https://cdn.mezon.ai/1779484504377790464/1827994776956309504/1827994776956309500/1749723253374_undefined1749703024201_0image.png_cropped@webp",
+  },
+
+  {
+    name: "Ta Quyen",
+    elo: 600,
+    location: "Yên Thành",
+    badge: "Rising Star",
+    img: "https://imgproxy.mezon.ai/K0YUZRIosDOcz5lY6qrgC6UIXmQgWzLjZv7VJ1RAA8c/rs:fit:100:100:1/mb:2097152/plain/https://cdn.mezon.vn/1779484504377790464/1833331797883097088/1831634827619602400/185_undefined113_Nguy_n_T__Quy_n.jpg@webp",
+  },
+  {
+    name: "Anh Tuan",
+    elo: 650,
+    location: "Diễn Châu",
+    badge: "Rising Star",
+    img: "https://imgproxy.mezon.ai/K0YUZRIosDOcz5lY6qrgC6UIXmQgWzLjZv7VJ1RAA8c/rs:fit:100:100:1/mb:2097152/plain/https://cdn.mezon.vn/1779484504377790464/1833699295371464704/1833682843671203800/748_undefinedIMG_3686__1_.jpg@webp",
+  },
+];
